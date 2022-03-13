75daba740c76aa11d21e7e1b3d1f2710a938d485
Micah Geisel <micah@botandrose.com>
2022-03-13 12:16:16 -0600

    remove jquery dependency in cells plugin.

--- app/assets/javascripts/ckeditor/plugins/cells/dialogs/cells.js
+++ app/assets/javascripts/ckeditor/plugins/cells/dialogs/cells.js
@@ -1,9 +1,4 @@
 CKEDITOR.dialog.add("cellDialog", function(editor) {
-  function getSelectedCell() {
-    let node = editor.getSelectedHtml().$.firstElementChild
-    return node if node?.nodeName == "CELL"
-  }
-
   return {
     title: "Insert Cell",
     minWidth: 400,
@@ -19,15 +14,13 @@ CKEDITOR.dialog.add("cellDialog", function(editor) {
           let select = event.sender
           let dialog = select.getDialog()
           let form = dialog.getContentElement("main", "form")
-          let cellConfig = dialog.cellConfigs.find((cellConfig) => {
-            cellConfig.id === event.data.value
-          })
+          let cellConfig = dialog.cellConfigs.find(config => config.id === event.data.value)
 
           form.getElement().setHtml(cellConfig.form)
 
           if(selectedCell = getSelectedCell()) {
-            $(form.getElement().$).find("[name]").each((_, element) => {
-              $(element).val selectedCell.getAttribute(element.name)
+            form.getElement().$.querySelectorAll("[name]").forEach(element => {
+              setValue(element, selectedCell.getAttribute(element.name))
             })
           }
         },
@@ -45,11 +38,10 @@ CKEDITOR.dialog.add("cellDialog", function(editor) {
         let select = this.getContentElement("main", "cellName")
         select.clear()
 
-        for(let i=0; i < this.cellConfigs.length; i++) {
-          let cellConfig = this.cellConfigs[i]
-          select.add(cellConfig.name, cellConfig.id)
-        }
-        if(let selectedCell = getSelectedCell()) {
+        this.cellConfigs.forEach(config => select.add(config.name, config.id))
+
+        let selectedCell = getSelectedCell()
+        if(selectedCell) {
           select.setValue(selectedCell.getAttribute("name"))
         }
       });
@@ -60,12 +52,74 @@ CKEDITOR.dialog.add("cellDialog", function(editor) {
       cell.setAttribute("name", this.getValueOf("main", "cellName"));
 
       let form = this.getContentElement("main", "form")
-      $(form.getElement().$).find("[name]").each((_, element) => {
-        cell.setAttribute(element.name, $(element).val())
+      form.getElement().$.querySelectorAll("[name]").forEach(element => {
+        cell.setAttribute(element.name, getValue(element))
       })
 
       editor.insertElement(cell)
     },
+  };
+
+  function getSelectedCell() {
+    let node = editor.getSelectedHtml().$.firstElementChild
+    if(node && node.nodeName == "CELL") {
+      return node 
+    }
+  }
+
+  function getValue(element) {
+    switch(element.type) {
+      case 'radio':
+      case 'checkbox':
+        const elements = document.querySelectorAll(`[name="${element.name}"]:checked`)
+        const values = Array.from(elements).map(e => e.value)
+        if(element.name.endsWith("[]")) {
+          return values
+        } else {
+          return values[0]
+        }
+      default:
+        return element.value
+    }
+  }
+
+  function setValue(element, value) {
+    if ('undefined' === typeof value) {
+      value = '';
+    }
+
+    if (null === value) {
+      value = '';
+    }
+
+    var type = element.type || element[0].type;
+
+    switch(type ) {
+      default:
+        element.value = value;
+        break;
+
+      case 'radio':
+      case 'checkbox':
+        element.checked = element.value === value;
+        break;
+
+      case 'select-multiple':
+        var values = value.constructor === Array ? value : [value];
+        for(var k = 0; k < element.options.length; k++) {
+          element.options[k].selected = (values.indexOf(element.options[k].value) > -1 );
+        }
+        break;
+
+      case 'select':
+      case 'select-one':
+        element.value = value.toString() || value;
+        break;
+
+      case 'date':
+        element.value = new Date(value).toISOString().split('T')[0];
+        break;
+    }
   };
 });
 
