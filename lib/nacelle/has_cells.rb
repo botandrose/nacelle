b2ce66f76974ffed70725ccb52a6cf372bbf2295
Micah Geisel <micah@botandrose.com>
2023-05-05 20:20:51 +0200

    fucking nil.

--- lib/nacelle/has_cells.rb
+++ lib/nacelle/has_cells.rb
@@ -3,7 +3,7 @@ module Nacelle
     def has_cells *columns
       define_method :cells do
         columns.flat_map do |column|
-          send(column).scan(/(<cell[^>]*\/\s*>|<cell[^>]*>.*?<\/cell>)/m).map do |matches|
+          send(column).to_s.scan(/(<cell[^>]*\/\s*>|<cell[^>]*>.*?<\/cell>)/m).map do |matches|
             tag = matches.first
             attrs = Hash.from_xml(tag)['cell']
             name, _ = attrs.delete('name').split('/')
