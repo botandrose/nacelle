deb71d870275d21c140d4798c97caa22fcd3d85b
Micah Geisel <micah@botandrose.com>
2023-05-05 18:44:37 +0200

    refactor cell rewriter from middleware to AC::after_filter.

--- lib/nacelle.rb
+++ lib/nacelle.rb
@@ -2,12 +2,12 @@ require "nacelle/version"
 require "action_controller/railtie"
 require "sprockets/railtie"
 require "nacelle/cell"
-require "nacelle/middleware"
+require "nacelle/after_filter"
 
 module Nacelle
   class Engine < ::Rails::Engine
     initializer "nacelle.init" do |app|
-      app.config.middleware.use Nacelle::Middleware
+      ActionController::Base.after_action AfterFilter
     end
 
     initializer "nacelle.assets.precompile" do |app|
---
+++ lib/nacelle/after_filter.rb
@@ -0,0 +1,40 @@
+module Nacelle
+  class AfterFilter < Struct.new(:controller)
+    def self.after controller
+      if controller.content_type.include?("text/html")
+        new(controller).call
+      end
+    end
+
+    def call
+      cells = cells controller.response.body
+
+      controller.response.body.gsub!(/(#{cells.keys.join('|')})/) do |tag|
+        name, state, attrs = cells[tag]
+        attrs = HashWithIndifferentAccess.new(attrs)
+        cell = "#{name.camelize}Cell".constantize.new_with_request(controller.request)
+        args = [state]
+        attrs.delete "class" # ignore styling class
+        args << attrs unless attrs.empty?
+        begin
+          cell.render_state *args
+        rescue AbstractController::ActionNotFound
+          "<strong>Cell “#{name.capitalize} #{state}” not found!</strong>"
+        end
+      end unless cells.empty?
+    end
+
+    private
+
+    def cells html
+      html.scan(/(<cell[^>]*\/\s*>|<cell[^>]*>.*?<\/cell>)/m).inject({}) do |cells, matches|
+        tag = matches.first
+        attrs = Hash.from_xml(tag)['cell']
+        name, state = attrs.delete('name').split('/')
+        cells[tag] = [name, state, attrs]
+        cells
+      end
+    end
+  end
+end
+
